62e66a2b7d19e0e36298e7ee863ad37ea58119eb
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-06 23:45:04 +0300

    exerecise 1-2-3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myarray, myvalue) {
+    for (let i = 0; i < myarray.length; i++) {
+        if (myarray[i] == myvalue)
+        {
+            
+        }
+    }
 };
 
 // Do not edit below this line
