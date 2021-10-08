737fc9baa91946ebe5150f4d3ccf9f29f03c294f
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-08 11:33:52 +0300

    6, 7 ex

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isleap = false;
+    if (year % 4 === 0)
+    {
+        isleap = true;
+    }        
+    if (year % 100 === 0)
+    {
+        if (year % 400 === 0) {
+            isleap = true;
+        }
+        else
+        {
+            isleap = false;
+        }
+    }
+    return isleap;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+  let output = (number-32)*(5.0/9.0)
+  return Math.round(output*10)/10
 };
 
-const ctof = function() {
-
+const ctof = function(number) {
+  let output = (number*9.0/5.0)+32
+  return Math.round(output*10)/10
 };
 
 // Do not edit below this line
