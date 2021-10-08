2dc4bb4dd87b58bf073a69199a6b0cb5bb578493
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-08 11:10:26 +0300

    ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,20 @@
-const removeFromArray = function(myarray, myvalue) {
+const removeFromArray = function(myarray, ...myvalue) {
+    let output = [];
+    let remove;
     for (let i = 0; i < myarray.length; i++) {
-        if (myarray[i] == myvalue)
+        remove = false;
+        for (let ii = 0; ii < myvalue.length; ii++) {
+            if (myarray[i] == myvalue[ii] && (typeof myarray[i] == typeof myvalue[ii]))
+            {
+                remove = true;
+            }    
+        }
+        if (!remove)
         {
-            
+            output.push(myarray[i]);
         }
     }
+    return output;
 };
 
 // Do not edit below this line
