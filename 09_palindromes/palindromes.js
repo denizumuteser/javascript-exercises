327d0e16990ac2f77f02c38d7a5dccf3ef67fe69
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-10 14:55:14 +0300

    ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,39 @@
-const palindromes = function () {
 
+function checkpunc(c)
+{
+    var punctuation = [".", ",", ":", "?", "!", ";", " "]
+    for (let i = 0; i < punctuation.length; i++) {
+        if (punctuation[i] == c) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const palindromes = function (s) {
+    
+    let forward = "";
+    let backward = "";
+    
+    for (let i = 0; i < s.length; i++) {
+        if (!checkpunc(s[i])) {
+            forward += s[i];
+        }
+        
+    }
+
+    for (let i = s.length-1; i >= 0; i--) {
+        if (!checkpunc(s[i])) {
+            backward += s[i];
+        }
+    }
+    if (forward.toLocaleLowerCase() === backward.toLocaleLowerCase()) {
+        return true;
+    }
+    return false;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
