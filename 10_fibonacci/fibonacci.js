21b41e0d0a57d6c340a7ed240048749512232024
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-10 15:01:42 +0300

    ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    let fibar = [1,1]
+    for (let i = 2; i < n; i++) {
+        let temp = fibar[i-2]+fibar[i-1];
+        fibar.push(temp);
+    }
+    return fibar[n-1];
 };
 
 // Do not edit below this line
