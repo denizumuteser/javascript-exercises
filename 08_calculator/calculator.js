11271f15edcf38b1d8478fdb3a5ada40a9013d3d
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-10 12:02:55 +0300

    ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  let mul = 1;
+  for (let i = 0; i < a.length; i++) {
+    mul *= a[i];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+	for (let i = 0; i < a; i++) {
+    sum *= i+1;
+  }
+  return sum;
 };
 
 // Do not edit below this line
